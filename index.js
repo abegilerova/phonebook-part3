1b16c646375be9cc7fad3ce18a4ac952087f6d21
abegilerova <abegilerova@gmail.com>
2019-11-25 14:37:28 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,108 @@
+//const http = require('http')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+//function that is used to create an express application stored in the app variable:
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(cors())
+
+
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
+
+
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 456
+    },
+
+    {
+        "name": "Aika Begilerova",
+        "number": "272148",
+        "id": 457
+    }
+]
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+let numberOfPeople = persons.length;
+let date = new Date();
+app.get('/info', (req, res) => {
+    res.json('Phonebook has info for ' + numberOfPeople + ' people ' + '\n ' + date)
+
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end();
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    console.log(body)
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'content'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+    persons = persons.concat(person)
+    res.json(person)
+
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+
+})
