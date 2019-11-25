f149d75607f835cef24a2c5a77affa51e1ba1077
abegilerova <abegilerova@gmail.com>
2019-11-25 16:27:01 -0500

    build folder for front end

---
+++ build/precache-manifest.d923bbe8456988e1d46a9ab444d053f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebabd8b7f1e9a3587812f7e3e2e064b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12f1ef6da4249bc84781",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e625db142a0c8559fc44",
+    "url": "/static/js/2.8ae619b4.chunk.js"
+  },
+  {
+    "revision": "12f1ef6da4249bc84781",
+    "url": "/static/js/main.cdfd5e5a.chunk.js"
+  },
+  {
+    "revision": "4e646f0e581801a589fc",
+    "url": "/static/js/runtime-main.7767e197.js"
+  }
+]);
